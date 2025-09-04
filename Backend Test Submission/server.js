2a10151de626b5c589eafd5244ac30f672fa2187
vardhan011 <borragovardhanreddy@gmail.com>
2025-09-04 11:09:37 +0530

    Add Backend Test Submission as normal folder

---
+++ Backend Test Submission/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Server is running" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+    
